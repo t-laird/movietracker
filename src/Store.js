ccef4e90fe4359ff8a03eeff3e229dc85f179645
David Ryan <djrdev@icloud.com>
2017-12-11 21:42:23 -0700

    final Store

--- src/Store.js
+++ src/Store.js
@@ -7,3 +7,16 @@ import {
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+
+import RootReducer from './Reducers';
+
+const middleware = applyMiddleware(thunk, promise, logger);
+
+const Store = createStore (
+  RootReducer,
+  compose(
+    middleware,
+  )
+);
+
+export default Store;
