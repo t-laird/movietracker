c3ecd12d2d8f485b90f2f0484c6c1a7659169375
Thomas Laird <t6r6l5@gmail.com>
2017-12-13 13:43:53 -0700

    signin reducer for success and failure cases

--- src/Reducers/SignIn.js
+++ src/Reducers/SignIn.js
@@ -1,3 +1,17 @@
-const signIn = (status) => {
-  
-}
+const SignIn = (state = {}, action) => {
+  switch(action.type) {
+    case 'SIGNIN_SUCCESS':
+      const { userObject } = action;
+      const dataResponse = userObject.data;
+      const userData = Object.assign({}, {name: dataResponse.name, email: dataResponse.email, id: dataResponse.id})
+      return {signedIn: true, userData};
+    case 'SIGNIN_FAILURE':
+    const { errorMessage } = action;
+      return {signedIn: false, error: errorMessage};
+    default:
+      return {signedIn: false};
+  }
+};
+
+export default SignIn;
+
