296a18a17c5d738e0c0161332848a1da5c446220
Thomas Laird <t6r6l5@gmail.com>
2017-12-13 19:53:51 -0700

    header stateful - use cmpwillupdate to change remove signin on success

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = (props) => {
+class Header extends Component{
+  constructor() {
+    super();
+  }
 
-  return (
-    <div className="Header">
-      <h1>Movie Tracker</h1>
-      <Link className="sign-in" to="/signin">Sign In/Sign Up</Link>
-    </div>
-  );
+  componentWillUpdate(nextProps, nextState) {
+    if (nextProps.location.pathname === '/signin' && nextProps.signedIn === true) {
+      this.props.history.push('/');
+    }
+  }
+
+  render() {
+    console.log(this.props);
+    const signedIn = this.props.signedIn;
+    const signInContent = signedIn
+      ? (<div className="sign-out"><span>Welcome, {this.props.userName}</span> <Link className="sign-in-link" to="/">Sign Out</Link></div>)
+      : <Link className="sign-in-link" to="/signin">Sign In/Sign Up</Link>;
+    
+    return (
+      <div className="Header">
+        <h1>Movie Tracker</h1>
+        <div className="sign-in">
+          {signInContent}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  signedIn: state.SignIn.signedIn,
+  userName: state.SignIn.userData.name
+});
+
+export default connect(mapStateToProps, null)(Header);
