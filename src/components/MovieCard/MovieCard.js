52260923654dd31441f2a1af2878a27d094b053f
Sam Singer <samuel.singer1@gmail.com>
2017-12-14 10:12:51 -0700

    Create the movie card container for the favorites

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { toggleFavorites } from '../../Actions';
 
 const MovieCard = (props) => {
+
   return (
     <div>
+      <button onClick={(event) => {
+        event.preventDefault();
+        props.toggleFavorites(props.movie)
+      }}>FAVORITE</button>
       <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt="movie poster" />
       <h1>{props.title}</h1>
     </div>
   )
 }
 
-export default MovieCard;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleFavorites: (movie) => dispatch(toggleFavorites(movie)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(MovieCard);
