cd64c7bd7d9916296a985f58054dd6e2e264314b
Thomas Laird <t6r6l5@gmail.com>
2017-12-13 19:53:25 -0700

    route header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Route path="/" component={Header} />
         <Route path="/" component={MovieContainer} />
         <Route path="/signin" component={SignIn} />
       </div>
