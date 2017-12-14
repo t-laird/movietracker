207f43cb35e110d6aba183d137df601e93334d57
Thomas Laird <t6r6l5@gmail.com>
2017-12-13 18:55:42 -0700

    add rouder to index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-//import MovieIndex from './components/MovieIndex';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './Reducers';
-//import configureStore from './store/configureStore';
 import Store from './Store';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={ Store }>
-    <App/>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
