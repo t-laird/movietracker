f575aa2d3bd2c98b7a43c342898e8e24a73e75dc
David Ryan <djrdev@icloud.com>
2017-12-14 23:00:25 -0700

    moved new card html into return

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,30 +5,23 @@ import { toggleFavorites } from '../../Actions';
 
 const MovieCard = (props) => {
 
-  <div id="f1_container">
-    <div id="f1_card" class="shadow">
-      <article class="front face">
-        <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt="movie poster" width="290" height="430" />
-      </article>
-      <article class="back face center">
-        <h1>{props.title}</h1>   
-        <p>{props.rating}</p>     
-        <p>{props.overview}</p>
-        <button onClick={(event) => {
-          props.toggleFavorites(props.movie)
-        }}>FAVORITE</button>
-      </article>
-    </div>
-  </div>
 
   return (
-    <article>
-      <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt="movie poster" width="290" height="430" />
-      <button onClick={(event) => {
-        props.toggleFavorites(props.movie)
-      }}>FAVORITE</button>
-      <h1>{props.title}</h1>
-    </article>
+    <div className="outer-card-container">
+      <div className="inner-card-container">
+        <article class="card-front face">
+          <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt="movie poster" width="290" height="430" />
+        </article>
+        <article class="card-back face">
+          <h1>{props.title}</h1>   
+          <p className="rating">{props.rating}</p>     
+          <p>{props.overview}</p>
+          <button onClick={(event) => {
+            props.toggleFavorites(props.movie)
+          }}>FAVORITE</button>
+        </article>
+      </div>
+    </div> 
   )
 }
 
