fd7f8a3c529d3b795fa6b8512d71b4e5cd1ae9d6
David Ryan <djrdev@icloud.com>
2017-12-14 18:36:41 -0700

    Revert "Card animation"

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,17 +5,6 @@ import { toggleFavorites } from '../../Actions';
 
 const MovieCard = (props) => {
 
-  // <div className="shadow">
-  //   <article>
-  //     <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt="movie poster" width="290" height="430" />
-  //     <h1>{props.title}</h1>
-  //   </article>
-  //   <article>
-  //     <p></p>
-  //     <p></p>
-  //   </article>
-  // </div> 
-
   return (
     <article>
       <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt="movie poster" width="290" height="430" />
