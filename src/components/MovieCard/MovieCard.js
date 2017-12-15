ba75cc0cdea68be03eedbcffa71cbe28c088517e
David Ryan <djrdev@icloud.com>
2017-12-14 21:55:03 -0700

    html structure for flip

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,22 +5,26 @@ import { toggleFavorites } from '../../Actions';
 
 const MovieCard = (props) => {
 
-  // <div className="shadow">
-  //   <article>
-  //     <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt="movie poster" width="290" height="430" />
-  //     <h1>{props.title}</h1>
-  //   </article>
-  //   <article>
-  //     <p></p>
-  //     <p></p>
-  //   </article>
-  // </div> 
+  <div id="f1_container">
+    <div id="f1_card" class="shadow">
+      <article class="front face">
+        <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt="movie poster" width="290" height="430" />
+      </article>
+      <article class="back face center">
+        <h1>{props.title}</h1>   
+        <p>{props.rating}</p>     
+        <p>{props.overview}</p>
+        <button onClick={(event) => {
+          props.toggleFavorites(props.movie)
+        }}>FAVORITE</button>
+      </article>
+    </div>
+  </div>
 
   return (
     <article>
       <img src={`https://image.tmdb.org/t/p/w500${props.poster}`} alt="movie poster" width="290" height="430" />
       <button onClick={(event) => {
-        event.preventDefault();
         props.toggleFavorites(props.movie)
       }}>FAVORITE</button>
       <h1>{props.title}</h1>
