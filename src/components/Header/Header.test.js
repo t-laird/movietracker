216ac6c8cdda5e16038362d76656fcc908fb02dc
Thomas Laird <t6r6l5@gmail.com>
2017-12-16 10:50:12 -0700

    tests for header component

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -5,11 +5,12 @@ import { Header } from './Header';
 
 describe('header tests', () => {
   let headerContainer;
+  let mockProps;
   beforeEach(() => {
-    const mockProps = {
+    mockProps = {
       signOut: jest.fn(),
       showFavorites: jest.fn(),
-      signedIn: false,
+      signedIn: true,
       userName: 'jim',
       shouldShowFavorites: true,
       favorites: 3
@@ -19,5 +20,29 @@ describe('header tests', () => {
 
   it('should render correctly', () => {
     expect(headerContainer).toBeDefined();
+  });
+
+  it('should be instantiated with the correct children', () => {
+    const expectedH1Length = 1;
+    const expectedH2Length = 1;
+    const expectedLinkLength = 2;
+
+    expect(headerContainer.find('h1').length).toEqual(expectedH1Length);
+    expect(headerContainer.find('h2').length).toEqual(expectedH1Length);
+    expect(headerContainer.find('Link').length).toEqual(expectedLinkLength);
+  });
+
+  it('should render the user\'s name if signedin is true', () => {
+    const expectedUserName = mockProps.userName;
+
+    expect(headerContainer.find('span').text()).toContain(expectedUserName);
+  });
+
+  it('should call signOut on click of a the signout link', () => {
+    const signOutButton = headerContainer.find('.sign-in-link');
+    
+    signOutButton.simulate('click');
+
+    expect(mockProps.signOut).toHaveBeenCalled();
   });
 });
