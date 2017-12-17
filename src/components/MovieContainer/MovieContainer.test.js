5443bb18e7dc72f7dcf8d7ffe603e94083c719d4
David Ryan <djrdev@icloud.com>
2017-12-16 17:26:35 -0700

    MovieContainer example tests adaptation

--- src/components/MovieContainer/MovieContainer.test.js
+++ src/components/MovieContainer/MovieContainer.test.js
@@ -1,7 +1,81 @@
-import React, { Component } from 'react';
-import MovieContainer from './MovieContainer';
-import { shallow } from 'enzyme';
-import Provider from 'react-redux';
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import { MovieContainer, mapStateToProps, mapDispatchToProps } from './MovieContainer'
+import * as actions from '../../Actions'
+
+describe('MovieContainer', () => {
+  describe('MovieContainer Component', () => {
+    it('should match the snapshot', () => {
+      const renderedComponent = shallow(<MovieContainer />)
+      expect(renderedComponent).toMatchSnapshot()
+    })
+
+    it('should call handleSubmit on submitForm', () => {
+      const mockHandleSubmit = jest.fn()
+      const mockEvent = { preventDefault: jest.fn() }
+      const renderedComponent = shallow(<AddTodoForm
+        handleSubmit={mockHandleSubmit}
+      />)
+      renderedComponent.instance().submitForm(mockEvent)
+      expect(mockHandleSubmit).toHaveBeenCalled()
+    })
+
+    it('should call handleRemove on removedClicked', () => {
+      const mockHandleRemove = jest.fn()
+      const mockEvent = { preventDefault: jest.fn() }
+      const renderedComponent = shallow(<AddTodoForm
+        handleRemove={mockHandleRemove}
+      />)
+      renderedComponent.instance().removedClicked(mockEvent, 0)
+      expect(mockHandleRemove).toHaveBeenCalledWith(0)
+    })
+
+    it('should build html when printTodos is called', () => {
+      const mockTodos = [{ text: 'Wrestle a wombat', id: 0 }]
+      const renderedComponent = shallow(<AddTodoForm
+        todos={mockTodos}
+      />)
+      const expected = [
+        <div key="todo-0">
+          <p>Wrestle a wombat</p>
+          <button onClick={(e) => renderedComponent.instance().removedClicked(e, 0)}>
+            Remove
+          </button>
+        </div>
+      ]
+      expect(renderedComponent.instance().printTodos()).toEqual(expected)
+    })
+  })
+
+  describe('mapStateToProps', () => {
+    it('should pull todos from the store', () => {
+      const mockStore = {
+        todos: [],
+      }
+      const result = mapStateToProps(mockStore)
+      expect(result.todos).toEqual(mockStore.todos)
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch when handleSubmit is called', () => {
+      const mockDispatch = jest.fn()
+      const mockTodo = { text: 'Pet a platypus', id: 0 }
+      const result = mapDispatchToProps(mockDispatch)
+      result.handleSubmit(mockTodo)
+      expect(mockDispatch).toHaveBeenCalledWith(actions.addTodo(mockTodo))
+    })
+
+    it('should call dispatch when handleRemove is called', () => {
+      const mockDispatch = jest.fn()
+      const result = mapDispatchToProps(mockDispatch)
+      result.handleRemove(0)
+      expect(mockDispatch).toHaveBeenCalledWith(actions.removeTodo(0))
+    })
+  })
+})
+
 
 describe('MovieContianer', () => {
   
