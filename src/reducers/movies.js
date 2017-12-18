78b3d0b826de4e034ce077694fb9d6d9d15dd44a
David Ryan <djrdev@icloud.com>
2017-12-18 08:56:29 -0700

    added initial state spread to each case statement

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -7,19 +7,19 @@ const initialState = {
 const movies = (state = initialState, action) => {
   switch (action.type) {
   case 'MOVIES_FETCH_DATA_SUCCESS':
-    return {
+    return { ...state,
       moviesIsLoading: false,
       movies: action.movies.results,
       moviesHasErrored: false
     };
   case 'MOVIES_IS_LOADING':
-    return {
+    return { ...state,
       moviesIsLoading: true,
       movies: [],
       moviesHasErrored: false
     };
   case 'MOVIES_HAS_ERRORED':
-    return {
+    return { ...state,
       moviesIsLoading: false,
       movies: [],
       moviesHasErrored: true
