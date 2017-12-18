c7849dae04cc3983b19c39d8627c7779666541c0
Sam Singer <samuel.singer1@gmail.com>
2017-12-17 23:23:36 -0700

    Removal of folder and more comments

--- src/Reducers/favorites.js
+++ src/Reducers/favorites.js
@@ -10,16 +10,3 @@ const favorites = (state=[], action) => {
 };
 
 export default favorites;
-
-// const todosReducer = (state = [], action) => {
-//   switch(action.type) {
-//     case 'ADD_TODO':
-//       return [ ...state, action.todo ]
-//     case 'REMOVE_TODO':
-//       return state.filter(todo => todo.id !== action.id)
-//     default:
-//       return state;
-//   };
-// };
-//
-// export default todosReducer;
